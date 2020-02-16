b019167813be768a0adb8b8e2e4446ffb36b1866
Aaron Joel <joe.lim.42.54@gmail.com>
2020-02-16 16:50:24 -0400

    Se agrega Login con css

---
+++ TicTacToe/main.js
@@ -0,0 +1,26 @@
+console.log("Todo bien");
+
+let turno = true;
+//x = true y o = false
+
+class position{
+    constructor(lugar){
+        this.ocupied = false;
+        this.place = lugar;
+    }
+}
+
+var cas1 = new position(document.getElementById("pos-1"));
+var cas2 = new position(document.getElementById("pos-2"));
+var cas3 = new position(document.getElementById("pos-3"));
+var cas4 = new position(document.getElementById("pos-4"));
+var cas5 = new position(document.getElementById("pos-5"));
+var cas6 = new position(document.getElementById("pos-6"));
+var cas7 = new position(document.getElementById("pos-7"));
+var cas8 = new position(document.getElementById("pos-8"));
+var cas9 = new position(document.getElementById("pos-9"));
+
+
+
+
+
