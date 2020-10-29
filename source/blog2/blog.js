11c5e17571d82b8f880e30618d04347f143f20a4
DevDuxAaron <relojvale25@gmail.com>
2020-10-28 23:29:36 -0400

    Archivos de blog

---
+++ source/blog2/blog.js
@@ -0,0 +1,12 @@
+var myString = "Hello my friend";
+
+var myStringOne = "Hello";
+var myStringTwo = "my friend";
+var myNewString = myStringOne.concat(myStringTwo);
+
+var greeting = "".concat("This","is","a","string");
+
+var greetingTwo = "Goodbye";
+greetingTwo += "my";
+greetingTwo += "friend";
+greetingTwo += "!!!"
